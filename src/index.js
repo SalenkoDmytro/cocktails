d1d72abf9d00f905951cb9ab44ca675124b06dd1
Maksym-Holovachuk <mgolovachuk@gmail.com>
2022-08-18 12:42:58 +0300

    add hbs & fetch

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import CocktailApiService from './js/services/CocktailApiService'
+// import { refs } from './js/services/refs';
+// import { appendImagesContainerEl } from './js/markup/markup'
+
+import headerTmp from './partials/gallery.hbs';
+
+const array = ["a", "b", "c"];
+
+console.log(headerTmp(array));
+
+
+
+// export async function onContainerClick(e) {
+//     const search = "Negroni"
+//     cocktailApiService.searchQuery = search;
+//     console.log(cocktailApiService.searchQuery);
+//     await cocktailApiService.fetchCocktaileByName();
+//     const { drinks } = cocktailApiService;
+
+//     const array = []
+//     for (let i = 0; i < 3; i++) {
+//         const randomDrink = await cocktailApiService.fetchRandomCocktaile()
+//         array.push(randomDrink[0]);
+//     }
+
+//     console.log(array);
+//     appendImagesContainerEl(array, refs.ul)
+// }
+
+// refs.container.addEventListener('click', onContainerClick)
+
+
---
+++ src/js/config/firebaseConfig.js
@@ -0,0 +1,9 @@
+// export const firebaseConfig = {
+//     apiKey: "AIzaSyCxgo4NPMcubYdu6-yKVDhXDmUWIEGZh6I",
+//     authDomain: "chat-bootcamp-93241.firebaseapp.com",
+//     databaseURL: "https://chat-bootcamp-93241-default-rtdb.europe-west1.firebasedatabase.app",
+//     projectId: "chat-bootcamp-93241",
+//     storageBucket: "chat-bootcamp-93241.appspot.com",
+//     messagingSenderId: "922391504238",
+//     appId: "1:922391504238:web:20ce0d8c95e4db53ebcd7f"
+// };
