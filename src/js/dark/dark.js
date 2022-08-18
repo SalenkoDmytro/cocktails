6fe7dba39bff5fe84eeacd7e16d95934b0e3414f
DenysKhm <khmelevskyy7@gmail.com>
2022-08-18 21:57:46 +0300

    com

---
+++ src/js/dark/dark.js
@@ -0,0 +1,7 @@
+// const themeSwitcher = document.querySelector('.toggle');
+
+// themeSwitcher.addEventListener('click', onThemeChange());
+
+// function onThemeChange() {
+//   body.style.backgroundColor = '#202025';
+// }
