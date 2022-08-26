b8ce5e6fe90bd186fc0d1485d260433efe37f416
SalenkoDmytro <dmytrosalenko1996@gmail.com>
2022-08-26 14:13:58 +0300

    1

--- src/js/favourites/renderFavorites.js
+++ src/js/favourites/renderFavorites.js
@@ -2,7 +2,6 @@ import { refs } from '../config/refs';
 import { cardsQuantity } from '../services/renderCards';
 import * as icons from '../../images/icons.svg';
 
-
 export function renderFavIngredients(array = []) {
   refs.gallery.innerHTML = '';
   const render = cardsQuantity(createFavIngMarkup(array));
@@ -10,8 +9,7 @@ export function renderFavIngredients(array = []) {
 }
 
 function createFavIngMarkup(data) {
-  console.log(data);
-  return data.map((data) => {
+  return data.map(data => {
     const { idIngredient, strIngredient, strType } = data[0];
     return `<li class="fav-ing__list-item card-set-item">
       <img class="ing-pict" src='https://www.thecocktaildb.com/images/ingredients/${strIngredient}-Small.png'
@@ -19,10 +17,10 @@ function createFavIngMarkup(data) {
         <p class="fav-ing__list-name">${strIngredient}</p>
         <p class="fav-ing__list-type">${strType}</p>
         <div class="fav-ing__btn-container">
-          <button class="fav-ing__btn fav-ing__btn-more" type="button">
+          <button class="fav-ing__btn fav-ing__btn-more" type="button" data-id="${idIngredient}" data-type='open-ingredient'>
             Learn more
           </button>
-          <button class="gallery__btn gallery__btn-fav js-btn-fav is-checked" type="button" data-id="${idIngredient}" data-favorite="ingredient">
+          <button class="gallery__btn gallery__btn-fav js-btn-fav is-checked" type="button" data-id="${idIngredient}" data-type='open-ingredient' data-favorite="ingredient">
             <span class="js-btn-gallery-text">Remove</span>
             <svg width="16" height="14" data-favorite class="gallery__btn-fav-svg is-checked">
               <use
@@ -35,10 +33,3 @@ function createFavIngMarkup(data) {
       </li>`;
   });
 }
-
-{/* <img
-  class="gallery__img"
-  src="${strDrinkThumb}"
-  alt="${strDrink}"
-  loading="lazy"
-/> */}
